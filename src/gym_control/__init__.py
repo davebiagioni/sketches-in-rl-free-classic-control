11f07ec4b9cf4ccc6bb68603c6d27a1b7dc6c0b8
Dave Biagioni <davebiagioni@users.noreply.github.com>
2022-04-23 15:09:55 -0600

    remove source code from gh-pages

--- src/gym_control/__init__.py
+++
@@ -1,3 +0,0 @@
-__version__ = "0.0.1"
-from gym_control.controller import GenericController
-from gym_control.runner import run_env
--- src/gym_control/args.py
+++
@@ -1,9 +0,0 @@
-import argparse
-
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    "--num-seeds", 
-    default=10,
-    type=int,
-    help="Number of seeds to use in evaluation"
-)
